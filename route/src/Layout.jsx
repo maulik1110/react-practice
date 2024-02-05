b5818a8370dfe0afa99d72c69402948ca28bcbbe
maulik1110 <maulik.monkenterprise@gmail.com>
2024-02-05 23:44:59 +0530

    route change

---
+++ route/src/Layout.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Footer from './components/Footer/Footer'
+import Header from './components/Header/Header'
+
+
+function Layout() {
+  return (
+    <>
+    {/* Layout */}
+    <Header/>
+
+    <Footer/>
+
+    </>
+  )
+}
+
+export default Layout
---
+++ route/src/main.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import Layout from './Layout.jsx'
+
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Layout/>
+  }
+])
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    {/* <App /> */}
+    <RouterProvider router={router}/>
+  </React.StrictMode>,
+)
